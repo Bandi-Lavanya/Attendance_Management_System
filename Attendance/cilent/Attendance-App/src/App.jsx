bd8bf6a0542aa344557e5a32e6cbf7ba7a789222
Bandi Lavanya <bandilavanya2004@gmail.com>
2024-12-11 10:03:18 +0530

    first commit

---
+++ Attendance/cilent/Attendance-App/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddAttendance from './components/AddAttendance';
+import DeleteStudent from './components/DeleteStudent';
+import GetStudentsData from './components/GetStudentsData';
+import Login from './components/Login'; 
+import Signup from './components/Signup'; 
+import StudentAttendance from './components/Student-Attendane'; // Ensure this path is correct
+import UpdateStudent from './components/UpdateStudent';
+import Home from './components/Home'; 
+import './App.css';
+import StudentAttendance_4th from './components/Student-Attendence-4th';
+import StudentHome from './components/studenthome';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+        <Route path=""  element={<Signup />} /> {/* Redirect default route */}
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} /> {/* Define a home route */}
+          <Route path="/studenthome" element={<StudentHome />} />
+          <Route path="/add-attendance" element={<AddAttendance />} />
+          <Route path="/delete-student" element={<DeleteStudent />} />
+          <Route path="/get-students-data" element={<GetStudentsData />} />
+          <Route path="/student-attendance/:id" element={<StudentAttendance />} />
+          <Route path="/student-attendance/4th-year" element={<StudentAttendance_4th />} />
+          <Route path="/update-student" element={<UpdateStudent />} />
+          <Route path="/update-student/:id" element={<UpdateStudent />} /> 
+          <Route path="/delete-student/:id" element={<DeleteStudent />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
