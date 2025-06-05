47591a2422e5bc6b4b1a35ba33d5f2bf21953485
Bandi Lavanya <142496325+Bandi-Lavanya@users.noreply.github.com>
2025-06-05 09:53:30 +0530

    Update api.js

--- Attendance/cilent/Attendance-App/src/api.js
+++ Attendance/cilent/Attendance-App/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000'; 
+const API_URL = 'https://attendance-management-system-backend-pj57.onrender.com'; 
 
 export const signup = async (data) => {
   return axios.post(`${API_URL}/signup`, data);
